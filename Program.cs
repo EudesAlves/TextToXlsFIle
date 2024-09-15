6a735606b8acce8faf517d12f717705637dd3266
Eudes <eudes.eds@gmail.com>
2024-09-15 07:36:21 -0300

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -8,25 +8,6 @@ void TransferTxtToExcelData()
     TextToExcelDataManager txtToExelManager = new TextToExcelDataManager();
     txtToExelManager.WriteTextFileDataToExcel();
     
-    /*
-    TxtDataManager txtManager = new TxtDataManager();
-    txtManager.GetDataFromTxtFiles();
-    txtManager.FillLinesOccurrencesCountInTextFiles();
-    txtManager.ShowLinesOccurrencesCountInTextFiles();
-
-    XlsDataManagerOffice xlsManager = new XlsDataManagerOffice();
-    string sheetName = "icemakerzero";
-    int row = 25;
-    int column = 8;
-    xlsManager.GetCellsFromColumn(sheetName, row, column);
-
-    xlsManager.WriteCellData( sheetName, 27, "H", "");
-    xlsManager.WriteCellData( sheetName, 28, "H", "");
-    xlsManager.WriteCellData( sheetName, 29, "H", "");
-    xlsManager.WriteCellData( sheetName, 30, "H", "");
-    xlsManager.WriteCellData( sheetName, 31, "H", "");
-    xlsManager.WriteCellData( sheetName, 32, "H", "");
-    */
     Console.ReadLine();
 }
 
--- XlsDataManagerOleDb.cs
+++ XlsDataManagerOleDb.cs
@@ -94,7 +94,5 @@ public class XlsDataManagerOleDb
                Console.WriteLine($"{record}");
             }
         }
-
-        
     }
 }
